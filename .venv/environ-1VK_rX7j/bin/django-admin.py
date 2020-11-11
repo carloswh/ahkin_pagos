088681ae5e1d307cd329513d648aab611fe66758
Victor <victor.wisphub@gmail.com>
2020-11-11 10:08:33 -0500

    fixed untracked files

--- .venv/environ-1VK_rX7j/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/victor-wisphub/Documentos/github/ahkin_pagos/.venv/environ-1VK_rX7j/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
