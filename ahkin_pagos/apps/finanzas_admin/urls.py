e96345d32d122bc350ab87b7cc00e1f0f13068e5
Victor <victor.wisphub@gmail.com>
2020-11-12 10:37:39 -0500

    Tabla pagos

--- ahkin_pagos/apps/finanzas_admin/urls.py
+++ ahkin_pagos/apps/finanzas_admin/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from ahkin_pagos.apps.finanzas_admin.views.dashboard_finanzas_admin_views import admin_finance_panel
 from ahkin_pagos.apps.finanzas_admin.views.facturas_views import FacturasListAdmin
+from ahkin_pagos.apps.finanzas_admin.views.pagos_views import PagosListAdmin
 
 urlpatterns = [
     #Dashboard
@@ -10,4 +11,7 @@ urlpatterns = [
 
     # Facturas
     url(r'^facturas/$', login_required(FacturasListAdmin.as_view()), name="lista_facturas"),
+
+    # Pagos
+    url(r'^pagos/$', login_required(PagosListAdmin.as_view()), name="lista_pagos"),
 ]
